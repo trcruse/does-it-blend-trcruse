b240914d4312264e04579fd794c21bf209c5bd46
trcruse <trcruse@F1092-26-2114>
2019-09-20 16:09:12 -0500

    does it blend

--- DoesItBlend/Fruit.cs
+++ DoesItBlend/Fruit.cs
@@ -7,10 +7,12 @@ namespace DoesItBlend
     /// <summary>
     /// A base class representing fruit
     /// </summary>
-    /// can't run abstract 
+    /// can't run abstract, virtual has implementation, abstract does not
     public abstract class Fruit : IBlendable
     {
         public abstract string Blend(); //abstract doesn't need a body
+
+
         
     }
 }
